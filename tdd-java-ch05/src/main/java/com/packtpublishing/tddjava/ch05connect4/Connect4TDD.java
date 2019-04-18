1dcae2d5f3782ebf6847ba51b7d0cdf9b36cf640
Michał Ciesielski <michal.ciesielski@softrender.pl>
2019-04-19 00:17:09 +0200

    Nearly finished ch 5

--- tdd-java-ch05/src/main/java/com/packtpublishing/tddjava/ch05connect4/Connect4TDD.java
+++ tdd-java-ch05/src/main/java/com/packtpublishing/tddjava/ch05connect4/Connect4TDD.java
@@ -1,15 +1,19 @@
 package com.packtpublishing.tddjava.ch05connect4;
 
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Connect4TDD {
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
+    public static final int DISCS_TO_WIN = 4;
+    private final PrintStream output;
 
     private Player[][] board = new Player[COLUMNS][ROWS];
     private int numberOfDiscs;
     private Player currentPlayer = Player.RED;
+    private Player winner = Player.EMPTY;
 
     public enum Player {
         RED('R'),
@@ -25,12 +29,11 @@ public class Connect4TDD {
             return String.valueOf(field);
         }
 
-
         private char field;
-
     }
 
-    public Connect4TDD() {
+    public Connect4TDD(PrintStream output) {
+        this.output = output;
         for (Player[] column : board) {
             Arrays.fill(column, Player.EMPTY);
         }
@@ -45,8 +48,12 @@ public class Connect4TDD {
         int disksInColumnCount = getNumberOfDisksInColumn(column);
         checkPositionToInsert(column, disksInColumnCount);
         insertDisc(column, disksInColumnCount);
-        switchPlayers();
+        if (didPlayerWin(disksInColumnCount, column)) {
+            setCurrentPlayerAsWinning();
+        }
+        switchPlayer();
         numberOfDiscs++;
+        printBoard();
         return disksInColumnCount;
     }
 
@@ -72,15 +79,81 @@ public class Connect4TDD {
         board[column][row] = currentPlayer;
     }
 
-    private void switchPlayers() {
-        if(currentPlayer == Player.BLUE) {
+    private void switchPlayer() {
+        if (currentPlayer == Player.BLUE) {
             currentPlayer = Player.RED;
-        } else  {
+        } else {
             currentPlayer = Player.BLUE;
         }
     }
 
     public Player getCurrentPlayer() {
+        output.println(currentPlayer + " player's turn");
         return currentPlayer;
+    }
+
+    private void printBoard() {
+        for (int y = ROWS - 1; y >= 0; y--) {
+            for (int x = 0; x < COLUMNS; x++) {
+                output.append('|').append(board[x][y].field);
+            }
+            output.append("|\n");
+        }
+    }
+
+    private boolean didPlayerWin(int row, int column) {
+        return checkVerticalWin(row, column) || checkHorizontalWin(row, column);
+    }
+
+    private Player[] getRow(int row) {
+        return Arrays.stream(board).map(column -> column[row]).toArray(Player[]::new);
+    }
+
+    private boolean checkVerticalWin(int row, int column) {
+        Player[] checkedColumn = board[column];
+        if (row >= DISCS_TO_WIN - 1) {
+            int offset = row - DISCS_TO_WIN + 1;
+            for (int index = 0; index < DISCS_TO_WIN; index++) {
+                if (checkedColumn[index + offset] != currentPlayer) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkHorizontalWin(int row, int column) {
+        Player[] theRow = getRow(row);
+        int startIdx = Math.max(0, column - DISCS_TO_WIN + 1);
+        int stopIdx = Math.min(COLUMNS - DISCS_TO_WIN, column);
+        for (int offset = startIdx; offset <= stopIdx; offset++) {
+            if(winsForOffset(theRow, offset)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Boolean winsForOffset(Player[] theRow, int offset) {
+        for (int idx = 0; idx < DISCS_TO_WIN; idx++) {
+            if (theRow[idx + offset] != currentPlayer) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void setCurrentPlayerAsWinning() {
+        winner = currentPlayer;
+    }
+
+    public boolean isFinished() {
+        return numberOfDiscs >= ROWS * COLUMNS;
+    }
+
+    public Player getWinner() {
+        return winner;
     }
 }
--- tdd-java-ch05/src/test/java/com/packtpublishing/tddjava/ch05connect4/Connect4TDDSpec.java
+++ tdd-java-ch05/src/test/java/com/packtpublishing/tddjava/ch05connect4/Connect4TDDSpec.java
@@ -5,8 +5,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.hamcrest.CoreMatchers.is;
+import java.io.*;
+
+import static com.packtpublishing.tddjava.ch05connect4.RegexMatcher.matches;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class Connect4TDDSpec {
@@ -14,10 +19,12 @@ public class Connect4TDDSpec {
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
+    private OutputStream output;
 
     @Before
     public void setUp() {
-        tested = new Connect4TDD();
+        output = new ByteArrayOutputStream();
+        tested = new Connect4TDD(new PrintStream(output));
     }
 
     @Test
@@ -28,7 +35,7 @@ public class Connect4TDDSpec {
     @Test
     public void whenOutsideTheBoardThenRuntimeException() {
         int[] columns = {-1, 7};
-        for(int column : columns) {
+        for (int column : columns) {
             exception.expect(RuntimeException.class);
             exception.expectMessage("Wrong column " + column);
             tested.putDiskInColumn(column);
@@ -58,7 +65,7 @@ public class Connect4TDDSpec {
     @Test
     public void whenNoMoreRoomInColumnThenRuntimeException() {
         int column = 3;
-        for(int i = 0; i < Connect4TDD.ROWS; i++) {
+        for (int i = 0; i < Connect4TDD.ROWS; i++) {
             tested.putDiskInColumn(column);
         }
 
@@ -77,5 +84,54 @@ public class Connect4TDDSpec {
         int column = 1;
         tested.putDiskInColumn(column);
         assertThat(tested.getCurrentPlayer(), is(Connect4TDD.Player.BLUE));
+    }
+
+    @Test
+    public void whenAskedForCurrentPlayerThenOutputNotice() {
+        tested.getCurrentPlayer();
+        assertThat(output.toString(), containsString(Connect4TDD.Player.RED + " player's turn"));
+    }
+
+    @Test
+    public void whenADiscIsIntroducedTheBoardIsPrinted() {
+        tested.putDiskInColumn(1);
+        assertThat(output.toString(), matches("(\\| \\| \\| \\| \\| \\| \\| \\|\\n){5}\\| \\|R\\| \\| \\| \\| \\| \\|\\n"));
+    }
+
+    @Test
+    public void whenGameStartsItIsNotFinished() {
+        assertFalse("A game cannot be finished!", tested.isFinished());
+    }
+
+    @Test
+    public void whenNoDiskCanBeIntroducedThenGameIsFinished() {
+        for (int row = 0; row < Connect4TDD.ROWS; row++) {
+            for (int col = 0; col < Connect4TDD.COLUMNS; col++) {
+                tested.putDiskInColumn(col);
+            }
+        }
+        assertTrue("The game has to be finished", tested.isFinished());
+    }
+
+    @Test
+    public void when4VerticalDiscsAreConnectedThanPlayerWins() {
+        for (int row = 0; row < 3; row++) {
+            tested.putDiskInColumn(1); // R
+            tested.putDiskInColumn(2); // B
+        }
+        tested.putDiskInColumn(1); // R
+        assertThat(tested.getWinner(), is(Connect4TDD.Player.RED));
+    }
+
+    @Test
+    public void when4HorizontalDiscsAreConnectedThenPlayerWins() {
+        tested.putDiskInColumn(Connect4TDD.COLUMNS - 1); // R
+        for (int i = 0; i < 3; i++) {
+            tested.putDiskInColumn(i + 1); // B
+            tested.putDiskInColumn(i + 1); // R
+        }
+        tested.putDiskInColumn(4); // B
+        System.out.println(output.toString());
+        assertThat(tested.getWinner(), is(Connect4TDD.Player.BLUE));
     }
 }
